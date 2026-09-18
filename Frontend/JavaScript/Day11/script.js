1e942914194be47880ffcc9e78cbfa756f1eb127
Divyansh Singh <divyanshsingh879596@gmail.com>
2026-09-18 22:48:24 +0530

    Add JavaScript Day1

---
+++ Frontend/JavaScript/Day11/script.js
@@ -0,0 +1,58 @@
+// console.log("Hello Divyansh!")
+// console.warn("JavaScript is Tough")
+// console.error("NO!!!!")
+// var a --> Declaration
+
+// var a = 10 --> Declaration + Initialisation
+// console.log(a)   -- > Used to show output 
+// var a = 10
+// console.log("Value of a is :",a)
+// var b  
+// b=  100
+// console.log("Value of b is :",b)
+// b=110
+// console.log("Value of the updated b is :",b)
+// console.log("Divyansh Singh")
+
+// var age = 19
+// var Age = 20
+// console.log(age)
+// console.log(Age)
+
+
+// ------DATATYPE-------
+// 1-Primitve
+// - Number : (10, 10.5 , +20, -36)
+// - String : ('a', "Divyansh", "Divyansh Singh")
+// - Boolean : (true , false)
+// - undefined  : only declaration not assigned 
+// - null : empty value at variable 
+// - BigInt : greater than range of Number(-(2^53 - 1) to +(2^53 - 1))
+// - Symbol : unique values
+// - 
+// 2-NonPrimitve
+// - Object
+// - Function
+// - Array
+
+// var a = 9999999999999922222222
+// console.log(a);
+
+
+// ----- ALERT-------
+// alert("This is Sex content")
+// alert("Khatra XXXXXX")
+
+//  --------CONFIRM--------
+// confirm("Are u 18 Plus")
+// var ans = confirm("Are u 18+")
+// console.log(ans)
+
+
+
+// -------PROMPT-------
+// var user = prompt("Enter your name :")
+// console.log(user)
+
+
+prompt("This is Demo")
