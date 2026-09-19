9d12e58173f1446bfb9aae29f7004294b1f93511
Divyansh Singh <divyanshsingh879596@gmail.com>
2026-09-19 11:32:44 +0530

    Add JavaScript Day13 practice

---
+++ Frontend/JavaScript/Day13/script.js
@@ -0,0 +1,40 @@
+// -----------WHILE LOOP-----
+// while(Condition){
+//     body
+// }
+// var a = 0 
+// while(a<11){
+//     console.log(a)
+//     a++
+// }
+
+// var num = Number(prompt("Enter the number whose table u want"))
+// var c = 1
+// while(c<=10){
+//     console.log(`${num} X ${c} : ${num * c}`)
+//     c++;
+// }
+
+
+// --------STRINGS-----
+// 
+var name = "Divyansh singh"
+var age = 22
+var a = "     ass   "
+// console.log("Length of :",name , name.length)
+// // console.log(name / age)
+// // console.log(`heros name is ${name} and hi ${age} year old`)
+
+// console.log(name.toUpperCase())
+// // console.log(name.toLowerCase())
+// // console.log(name.indexOf('h'))
+// // console.log(name.indexOf("ansh"))
+// // console.log(name.includes('anshi'))
+// console.log(name.slice(0,5))
+// console.log(name.substring())
+// console.log(name.replace('singh',"Singh"))
+// console.log(name.split(''))
+console.log(a);
+console.log(a.trim());
+console.log(name.repeat(2))
+console.log(name.charAt(0))
