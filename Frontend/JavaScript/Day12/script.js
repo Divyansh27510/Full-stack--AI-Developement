2b333c3327fa4b9ab8eb8f6b9a83fd08de8ee52c
Divyansh Singh <divyanshsingh879596@gmail.com>
2026-09-19 11:32:22 +0530

    Add JavaScript Day12 practice

---
+++ Frontend/JavaScript/Day12/script.js
@@ -0,0 +1,75 @@
+// console.log("Divyansh singh")
+// var s1 = Symbol("id");
+// var s2 = Symbol("id");
+
+// console.log(s1); // false
+
+// alert("Divyansh only few days left")
+// var a = confirm("Your age >= 18")
+// console.log(a)
+// var age = prompt("Enter your age") --- only string 
+
+
+// ------------TYPE CASTING-----------------
+
+// console.log("Ohh ur age is :",age)
+// var a = prompt("Enter the number")
+// var b = Number(a)
+
+// console.log(a,typeof(a))
+// console.log(b,typeof(b))
+
+// - 1:Explicit
+// var a  = prompt("Enter number1 ")
+// var b  = prompt("Enter number2 ")
+
+// var a1  = Number(a)
+
+// var b1  =Number(b)
+// console.log(a1+b1);
+
+
+
+//  ---------------TYPE OF OPERATORS ------------------
+// 1- Binary Operators
+//     - Arithmatic
+//     - Assignment 
+//     - Unary
+//     - Comparison 
+//     - Logical 
+
+
+// ------------CONDITIONALS------------
+// var age = prompt("Enter your age")
+// if(age >= 18){
+//     alert("Congrats You can Vote!")
+// }
+// else{
+//     alert("OOPS you cant vote!")
+// }
+
+// var maths = Number(prompt("Enter the maths marks"))
+// var phy = Number(prompt("Enter the phy marks"))
+// var chem = Number(prompt("Enter the chem marks"))
+// var avg = (maths + phy + chem)/3
+// console.log(`average marks is ${avg}`);
+
+// alert(`OHH your average marks is: ${avg}`)
+// if(avg >= 90){
+//     alert("You will Get Scholarship")
+// }
+// else{
+//     alert("OOPS you ahve to do more study")
+// }
+
+
+
+//  -------TRUHTY/FALSY------
+// -FALSY: false,0,Nan,null,undefined , ""
+// -Truthy: Evrything else is truthy value
+
+// -----------Ternary----------
+// console.log(10>51?'Sahi hai ':'nahi ye galat hai ')
+
+
+// 
